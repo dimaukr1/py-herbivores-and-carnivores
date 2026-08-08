9e0752fc918bef64e4c3c0c78b3edbf39d3ae6f3
Gachi_Dima <dimitriusdima4@gmail.com>
2026-08-08 14:49:37 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 class Animal:
     alive = []
 
-    def __init__(self, name, health=100, hidden=False) -> None:
+    def __init__(
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False,
+    ) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
@@ -9,21 +14,23 @@ class Animal:
 
     def __repr__(self) -> str:
         return (
-            f"Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}"
+            f"Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}"
         )
 
     def hide(self) -> None:
         self.hidden = not self.hidden
 
 
+class Herbivore(Animal):
+    def hide(self) -> None:
+        super().hide()
+
+
 class Carnivore(Animal):
-    def bite(self, animal) -> None:
+    def bite(self, animal: Herbivore) -> None:
         if isinstance(animal, Herbivore) and not animal.hidden:
             animal.health -= 50
             if animal.health <= 0:
                 Animal.alive.remove(animal)
-
-
-class Herbivore(Animal):
-    def hide(self) -> None:
-        super().hide()
